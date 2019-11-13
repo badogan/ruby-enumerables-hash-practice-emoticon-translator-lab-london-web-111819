ded170e985e10dd92f56864c9b6c2c10ed193507
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-13 21:10:11 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,18 +9,7 @@ def load_library(path)
     dictionary[:get_meaning][emoticon_array[1]] = key
     dictionary[:get_emoticon][emoticon_array[0]] = emoticon_array[1]
   end
-  p dictionary
   dictionary
-end
-
-def get_japanese_emoticon(path,western_emoticon)
-  dictionary = load_library(path)
-  sorry_message = "Sorry, that emoticon was not found"
-  if dictionary[:get_emoticon].include?(western_emoticon)
-    return dictionary[:get_emoticon][western_emoticon]
-  else
-    p sorry_message
-  end
 end
 
 def get_english_meaning(path,japanese_emoticon)
@@ -33,9 +22,20 @@ def get_english_meaning(path,japanese_emoticon)
   end
 end
 
-#p get_japanese_emoticon("angel")
-#p get_japanese_emoticon("wink")
+def get_japanese_emoticon(path,western_emoticon)
+  dictionary = load_library(path)
+  sorry_message = "Sorry, that emoticon was not found"
+  if dictionary[:get_emoticon].include?(western_emoticon)
+    return dictionary[:get_emoticon][western_emoticon]
+  else
+    p sorry_message
+  end
+end
+
+#main starts
+#===========
 path_from_main = './lib/emoticons.yml'
-#load_library(path_from_main)
 p get_japanese_emoticon(path_from_main,":)")
 p get_english_meaning(path_from_main,get_japanese_emoticon(path_from_main,":)"))
+get_japanese_emoticon(path_from_main,"Basri")
+#===========
